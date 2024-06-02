9d8ac91c4cd8aab5abaf695b2880fe2b4b46a118
Manthan Patidar <manthanpatidar@gmail.com>
2024-06-02 14:53:24 +0530

    initial commit

---
+++ src/main/java/com/manthan/springresouceserver/SpringResouceServerApplication.java
@@ -0,0 +1,13 @@
+package com.manthan.springresouceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResouceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResouceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manthan/springresouceserver/SpringResouceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manthan.springresouceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResouceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
